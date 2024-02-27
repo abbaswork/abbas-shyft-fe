f0aa57a8db134856a83d8ce560b48a2b1c255d1d
Abbas Khan <abbaskhan@Abbass-MacBook-Air.local>
2024-02-27 15:04:31 -0500

    feat: app with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import NavBar from "./components/navbar/navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from "./pages/Home";
+import { CreateStudentsPage } from "./pages/NewStudents";
+import { StudentsList } from "./pages/StudentsList";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+
+  //setup links
+  const links = [
+    { label: 'Home', href: '/' },
+    { label: 'Add New Students', href: '/new-students' },
+    { label: 'Students List', href: '/students-list' },
+    { label: 'Add New Courses', href: '/new-courses' },
+    { label: 'Courses List', href: '/courses-list' },
+    { label: 'Add New Results', href: '/new-results' },
+    { label: 'Results List', href: '/results-list' }
+  ];
+
+  return (
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <NavBar links={links} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new-students" element={<CreateStudentsPage />} />
+          <Route path="/students-list" element={<StudentsList />} />
+          <Route path="/new-courses" element={<CreateStudentsPage />} />
+          <Route path="/courses-list" element={<StudentsList />} />
+          <Route path="/new-results" element={<CreateStudentsPage />} />
+          <Route path="/results-list" element={<StudentsList />} />
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
+  );
+}
